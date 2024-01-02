1c3c141c8b0166a2380f76d86074082c7aa0b233
Merouane Kanoune <kanounemerouane@gmail.com>
2024-01-02 03:48:46 +0100

    Fixed some issue

--- app/components/pages/Job.tsx
+++ app/components/pages/Job.tsx
@@ -11,10 +11,10 @@ const jobData = [
     endDate: "2023-07",
     description: [
       "Ornidex, a Salesforce integrator. As part of Ornidex's development, the company wishes to implement a web application to retrieve leads and define a matching algorithm to automate the connection between customer needs and the profiles available in the database.",
-      "• Conducted a comprehensive analysis of the prevailing business challenges to gain a profound understanding of the issues at hand.",
-      "• Formulated the functional and technical design parameters for the proposed solution.",
-      "• Created the web application while simultaneously developing the associated matching algorithm.",
-      "• Executed testing and validation procedures to ensure the effectiveness and accuracy of the solution, guaranteeing a seamless user experience.",
+      "Conducted a comprehensive analysis of the prevailing business challenges to gain a profound understanding of the issues at hand.",
+      "Formulated the functional and technical design parameters for the proposed solution.",
+      "Created the web application while simultaneously developing the associated matching algorithm.",
+      "Executed testing and validation procedures to ensure the effectiveness and accuracy of the solution, guaranteeing a seamless user experience.",
     ],
     logo: "/Jobs/ornidex.jpg",
     url: "https://ornidex.com/",
@@ -27,12 +27,12 @@ const jobData = [
     endDate: "2023-02",
     description: [
       "Worked with the Direct sales team to build a B2B Tool. This tool is a solution that meets the need to implement a B2B sales and portfolio management tool, which manages sales forecasts, organizes the salesperson/client relationship, and improves team productivity by streamlining all stages of the sale, from managing opportunities to contract signing, as well as monitoring existing client portfolios.",
-      "• Selected as one of the top candidates from a pool of over 500 highly qualified applicants, demonstrating exceptional talent and expertise.",
-      "• Conducting an in‑depth analysis of the direct sales team’s domain expertise while meticulously gathering all pertinent information.",
-      "• Crafting a user experience/user interface (UX/UI) design for the application tailored to the specific requirements.",
-      "• Formulating the technical architecture for the solution as well as the structure of a relational database.",
-      "• Developing the solutions (Mobile application, front end, and back end for the web dashboard).",
-      "• Testing and deploying the solution once all the tests are passed and validated.",
+      "Selected as one of the top candidates from a pool of over 500 highly qualified applicants, demonstrating exceptional talent and expertise.",
+      "Conducting an in‑depth analysis of the direct sales team’s domain expertise while meticulously gathering all pertinent information.",
+      "Crafting a user experience/user interface (UX/UI) design for the application tailored to the specific requirements.",
+      "Formulating the technical architecture for the solution as well as the structure of a relational database.",
+      "Developing the solutions (Mobile application, front end, and back end for the web dashboard).",
+      "Testing and deploying the solution once all the tests are passed and validated.",
     ],
     logo: "/Jobs/djezzy.png",
     url: "https://www.djezzy.dz/",
@@ -45,8 +45,8 @@ const jobData = [
     endDate: "2023-06",
     description: [
       "Atlas Marketspace, a platform designed to revolutionize e-commerce. It offers a suite of tools enabling individuals and businesses to easily create, manage, and grow online stores without coding skills.",
-      "• Executed the implementation of front-end components and page designs.",
-      "• Collaborated with a team of 4 developers to build the project from scratch.",
+      "Executed the implementation of front-end components and page designs.",
+      "Collaborated with a team of 4 developers to build the project from scratch.",
     ],
     logo: "/Jobs/atlas.png",
     url: "https://www.atlas-marketspace.com/",
@@ -59,11 +59,11 @@ const jobData = [
     endDate: "2021-07",
     description: [
       "In the position, I worked on building a website to present the company and to help them share their updates in order to make the company more visible on the internet as well as acquire new customers.",
-      "• Analysing the needs of the company and defining the website’s features and functionalities.",
-      "• Identifying the website’s technical requirements, such as the platform, programming languages, and databases to be used.",
-      "• Developing the front‑end and back‑end components of the website, as well as integrating third‑party tools and services.",
-      "• Testing the website to ensure that it meets the business and user requirements.",
-      "• Deploying the website to a production environment, configuring the server, and testing the website again to ensure that it is working properly.",
+      "Analysing the needs of the company and defining the website’s features and functionalities.",
+      "Identifying the website’s technical requirements, such as the platform, programming languages, and databases to be used.",
+      "Developing the front‑end and back‑end components of the website, as well as integrating third‑party tools and services.",
+      "Testing the website to ensure that it meets the business and user requirements.",
+      "Deploying the website to a production environment, configuring the server, and testing the website again to ensure that it is working properly.",
     ],
     logo: "/Jobs/gica.png",
     url: "https://cetim-dz.com/",
@@ -76,11 +76,11 @@ const jobData = [
     endDate: "2022-07",
     description: [
       "GO PLATFORM is a Talent Acquisition marketplace built for Tech companies to take the pain of digital talent shortage. We are bringing a better way to source, screen, and recruit digital skills in Africa. We secure diversified, cheaper, and qualitative talents compared to the overpriced first-world talent.",
-      "• Executed the implementation of front-end components and page designs.",
-      "• Collaborated with a team of 4 developers to build the new version of the platform from scratch.",
-      "• Communicating with the executive board to decide which features to implement.",
-      "• Developing the platform’s backend and integrating the backend with the front end.",
-      "• Continuously integrating and deploying the new features that we have implemented, while the platform is working with 20,000 users.",
+      "Executed the implementation of front-end components and page designs.",
+      "Collaborated with a team of 4 developers to build the new version of the platform from scratch.",
+      "Communicating with the executive board to decide which features to implement.",
+      "Developing the platform’s backend and integrating the backend with the front end.",
+      "Continuously integrating and deploying the new features that we have implemented, while the platform is working with 20,000 users.",
     ],
     logo: "/Jobs/goplatform.png",
     url: "https://go-platform.com/",
@@ -132,14 +132,13 @@ const Job = () => {
                     </span>
                   )}
                 </time>
-                {data.description.map((paragraph, index) => (
-                  <p
-                    key={index}
-                    className="tracking-tight dark:text-zinc-400 text-zinc-600 my-2"
-                  >
-                    {paragraph}
-                  </p>
-                ))}
+                <div className="mt-8 dark:text-zinc-400 text-zinc-600 leading-relaxed">
+                  <ul className="list-disc ">
+                    {data.description.map((role, index) => (
+                      <li key={index}>{role}</li>
+                    ))}
+                  </ul>
+                </div>
               </div>
             </div>
           ))}
